778b75d5953e5f2bb9ef95fff753f5bd779bfa3e
frankirito <francoishenri.dupuich@gmail.com>
2016-07-02 15:14:05 +0200

    +gulped

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var wiredep = require('wiredep').stream;
+var sass = require('gulp-sass');
 
 gulp.task('wiredep', function () {
   gulp.src('*.html')
@@ -8,3 +9,14 @@ gulp.task('wiredep', function () {
     }))
     .pipe(gulp.dest('./'));
 });
+
+
+gulp.task('sass', function(){
+  return gulp.src('app/scss/style.scss')
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(gulp.dest('./'))
+});
+
+gulp.task("default", ["sass"], function() {
+  gulp.watch("app/scss/style.scss", ["sass"])
+})
