e0ed1bd0a46430ebde75e82c1c8d2a793e0ea860
frankirito <francoishenri.dupuich@gmail.com>
2016-07-09 16:55:53 +0200

    mixin proprieties

--- main.js
+++ main.js
@@ -11,6 +11,38 @@
     }
     ChangeIt();
 
+    // localStorage data task init
+    $(document).ready(function() {
+    var i = 0;
+    for (i = 0; i < localStorage.length; i++) {
+    var taskID = "task-" + i;
+    $('#taskList').append("<li id='" + taskID + "'>" + localStorage.getItem(taskID) + "</li>");
+    }
+    // Clear all task
+    $('#clear').click(function() {
+    localStorage.clear();
+    location.reload();
+    });
+    // Creation style, localStorage sauvegarde & generation li
+    $('#taskEntryForm').submit(function() {
+    if ($('#taskInput').val() !== "") {
+    var taskID = "task-" + i;
+    var taskMessage = $('#taskInput').val();
+    localStorage.setItem(taskID, taskMessage);
+    $('#taskList').append("<li class='ui-state-default ui-sortable-handle' id='" + taskID + "'>" + taskMessage + "</li>");
+    var task = $('#' + taskID);
+    task.css('display', 'none');
+    task.slideDown();
+    $('#taskInput').val("");
+    i++;
+    }
+    return false;
+    });
+    // Creation style
+    $(document).on('click', 'li', function() {
+    $(this).toggleClass('done').show("slow");
+    });
+    });
     // jQueryUI draggable+sortable
     $(function() {
     $("ul.droptrue").sortable({
@@ -22,5 +54,6 @@
     });
     $("#sortable1, #sortable2, #sortable3").disableSelection();
     });
+
 
 
