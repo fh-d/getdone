58e2589185281d1a3acb1ca31bdf82b19fac6d30
frankirito <francoishenri.dupuich@gmail.com>
2016-07-09 15:37:15 +0200

    minified css and js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var wiredep = require('wiredep').stream;
 var sass = require('gulp-sass');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
 
 gulp.task('wiredep', function () {
   gulp.src('*.html')
@@ -23,3 +25,10 @@ gulp.task('sass', function(){
  gulp.task('watch', function() {
   gulp.watch('app/scss/*.scss', ['sass'])
  });
+
+gulp.task('mini', function () {
+	gulp.src('app/css/style.css')
+		.pipe(cssmin())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('app/css'));
+});
